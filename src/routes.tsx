84264d36bfdc411ea3e789d032edc4d3eb13a658
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 08:08:31 +0200

    Change routes added new rout Currenct and Currency/CurrencyCode

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,21 @@
+import React from 'react';
 import { RouteProps } from 'react-router-dom';
 import Layout from './components/Layout';
 import NewsList from './features/news/';
 import InfoPage from './components/info';
-import Article from './features/news/article';
+
 const routes: RouteProps[] = [
     {
         path: '/',
         element: <Layout><InfoPage /></Layout>,
     },
     {
-        path: '/country',
+        path: '/currency',
         element: <Layout><InfoPage /></Layout>,
     },
     {
-        path: '/country/:countryCode',
+        path: '/currency/:currencyCode',
         element: <Layout><NewsList /></Layout>,
-    },
-    {
-        path: '/country/:countryCode/:title',
-        element: <Layout><Article  /></Layout>,
     },
 ];
 
