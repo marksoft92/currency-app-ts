912f73f5687e493c3a58e89847183773b711b01c
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 11:42:59 +0200

    refactor: Improve structure and styling in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,19 +4,18 @@ import Footer from './footer';
 import SideMenu from './SideMenu';
 import Breadcrumbs from './Breadcrumbs';
 
-
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <div>
+        <div className="layout">
             <Header />
             <Breadcrumbs />
             <div className="content">
                 <SideMenu />
-                <div className="main">{children}</div>
+                <main className="main-content">{children}</main>
             </div>
             <Footer />
         </div>
