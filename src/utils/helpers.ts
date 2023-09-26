dc569a31a9f040608ffb685dd8b119c42c1889ab
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 08:19:03 +0200

    Added new helpers to get Start and End date for timeseries api

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -17,3 +17,16 @@ export const convertToHyphenCase = (str: string) => {
     return str.replace(/\s+/g, '-').toLowerCase()
  }
 
+ export function getStartAndEndDate() {
+    const currentDate = new Date();
+    const currentDayOfWeek = currentDate.getDay();
+    const daysUntilPreviousMonday = (currentDayOfWeek + 6) % 7;
+
+    const start = new Date(currentDate);
+    start.setDate(currentDate.getDate() - daysUntilPreviousMonday - 7);
+
+    const startFormatted = start.toISOString().substring(0, 10);
+    const endFormatted = currentDate.toISOString().substring(0, 10);
+
+    return { start: startFormatted, end: endFormatted };
+}
