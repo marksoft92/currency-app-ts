2492f4abc82a7c398300e835f01ef34cea12620d
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 11:52:30 +0200

    refactor:  use default props for size and color and introduce constants for CSS variables

--- src/components/spiner.tsx
+++ src/components/spiner.tsx
@@ -5,12 +5,19 @@ interface LoaderProps {
     color?: string;
 }
 
-const Loader: React.FC<LoaderProps> = ({ size = 32, color = '#000' }) => {
+const LOADER_SIZE = '--size';
+const LOADER_COLOR = '--color';
+
+const Loader: React.FC<LoaderProps> = ({size, color}) => {
     return (
-        <div className="loader" style={{ '--size': `${size}px`, '--color': color } as React.CSSProperties}>
+        <div className="loader" style={{ [LOADER_SIZE]: `${size}px`, [LOADER_COLOR]: color } as React.CSSProperties}>
             <div className="loader-spinner"></div>
         </div>
     );
 };
 
+Loader.defaultProps = {
+    size: 32,
+    color: '#000',
+};
 export default Loader;
