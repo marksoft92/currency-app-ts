f62fb1356a5f52bc1cf98341ed35a35eb602ddd0
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 08:10:25 +0200

    fixed Store from another project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import countriesReducer from '../features/countries/countriesSLice';
-import viewSort from '../features/globalSlice/headerSlice';
+
 import newsRecuer from '../features/news/newsSlice'
 import languagesReducer from '../features/languages/languagesSlice';
 
 export const store = configureStore({
   reducer: {
     countries: countriesReducer,
-    view: viewSort,
     news: newsRecuer,
     languages: languagesReducer,
   },
