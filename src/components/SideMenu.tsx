8872a7e4aa57432cea4e3f3714d982306e7de1e6
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 08:13:07 +0200

    using code from a previous application, changing the code for a new project

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
@@ -12,37 +12,41 @@ const SideMenu: React.FC = () => {
     const { countries, loading } = useAppSelector((state) => ({
         countries: selectCountries(state),
         loading: selectLoading(state)
-      }));
-      console.log('xxxx',countries)
+    }));
     const dispatch = useAppDispatch();
     const locale: string = useAppSelector(selectLanguage);
     const localeShort = locale === 'en' ? 'bre' : locale === "pl" ? 'pol' : "bre"
-
 
     useEffect(() => {
         dispatch(fetchCountries());
     }, [dispatch]);
 
+    const [visibleItems, setVisibleItems] = useState(10); // Liczba widocznych pozycji początkowo
+    const increment = 10; // Liczba pozycji do wczytania za każdym razem
+
+    const loadMore = () => {
+        // Zwiększ liczbę widocznych pozycji o wartość "increment"
+        setVisibleItems(prevVisibleItems => prevVisibleItems + increment);
+    };
+
     return (
-        <Menu mode="inline">
-
-            {loading ? (
-                <Menu.Item key="loading" icon={<Spin indicator={<LoadingOutlined />} />}>
-                    <FormattedMessage id="spiner.loading" />
-                </Menu.Item>
-            ) : (
-                (countries?.[0]?.rates || []).map((country) => (
-
-                    <Menu.Item key={country.currency} >
-                        <Link to={`/country/${country.currency.toLowerCase()}`}>{`Currency: ${country.currency} Rate: ${country.rate}`}</Link>
+        <div>
+            <Menu mode="inline">
+                {loading ? (
+                    <Menu.Item key="loading" icon={<Spin indicator={<LoadingOutlined />} />}>
+                        <FormattedMessage id="spiner.loading" />
                     </Menu.Item>
-                ))
-            )}
-
-        </Menu>
+                ) : (
+                    (countries?.[0]?.rates || []).slice(0, visibleItems).map((country) => (
+                        <Menu.Item key={country.currency} >
+                            <Link to={`/currency/${country.currency.toLowerCase()}`}>{`Currency: ${country.currency} Rate: ${country.rate}`}</Link>
+                        </Menu.Item>
+                    ))
+                )}
+            </Menu>
+            <button onClick={loadMore}>Load More</button>
+        </div>
     );
 };
 
 export default SideMenu;
-
-
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
-import { useAppSelector } from '../app/hooks';
-import { selectArticles } from '../features/news/newsSlice';
+
 import { FormattedMessage } from 'react-intl';
 
 const Footer: React.FC = () => {
     const [currentTime, setCurrentTime] = useState<string>(dayjs().format('HH:mm:ss'));
-    const articles = useAppSelector(selectArticles);
-    const articlesCount = articles.length;
+
+
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrentTime(dayjs().format('HH:mm:ss'));
@@ -18,7 +17,7 @@ const Footer: React.FC = () => {
 
     return (
         <footer>
-            <p><FormattedMessage id="footer.count" /> {articlesCount}</p>&nbsp;
+
             <p><FormattedMessage id="footer.currentTime" /> {currentTime}</p>
         </footer>
     );
