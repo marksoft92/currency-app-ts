61ecb669abe913806c2bb0d84aeddf5fdd4cf043
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-09-26 11:35:55 +0200

    refactor: Separate time logic

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,22 +2,20 @@ import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
 import { FormattedMessage } from 'react-intl';
-
+const getCurrentTime = () => dayjs().format('HH:mm:ss');
 const Footer: React.FC = () => {
-    const [currentTime, setCurrentTime] = useState<string>(dayjs().format('HH:mm:ss'));
-
+    const [currentTime, setCurrentTime] = useState<string>(getCurrentTime());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCurrentTime(dayjs().format('HH:mm:ss'));
+            setCurrentTime(getCurrentTime());
         }, 1000);
 
         return () => clearInterval(intervalId);
     }, []);
-
+    
     return (
         <footer>
-
             <p><FormattedMessage id="footer.currentTime" /> {currentTime}</p>
         </footer>
     );
